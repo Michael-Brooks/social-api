1bc3af07e6288c06c0d73de779474e7472128019
Michael Brooks <Michael Brooks>
2016-08-29 12:38:40 +0100

    Added created response for friend request

--- app/Http/Controllers/AuthenticatedController.php
+++ app/Http/Controllers/AuthenticatedController.php
@@ -64,6 +64,8 @@ class AuthenticatedController extends Controller
         $friend = $user::username($username);
 
         $user->addFriend($friend);
+
+        return $this->response->created();
     }
 
     public function approveFriendRequest(Request $request, $username)
