5e0a52b100de6844c8713aaf17596c76b29ff666
Michael Brooks <me@michaelbrooks.co.uk>
2016-08-31 20:54:26 +0100

    Added in some factories for testing

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,9 +13,19 @@
 
 $factory->define(App\User::class, function ($faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => str_random(10),
-        'remember_token' => str_random(10),
+        'name'              => $faker->name,
+        'username'          => str_random(25),
+        'email'             => $faker->email,
+        'password'          => str_random(10),
+        'remember_token'    => str_random(10),
+    ];
+});
+
+$factory->define(App\StatusUpdate::class, function ($faker) {
+    return [
+        'message'   => $faker->paragraph,
+        'user_id'   => function () {
+            return factory(App\User::class)->create()->id;
+        }
     ];
 });
